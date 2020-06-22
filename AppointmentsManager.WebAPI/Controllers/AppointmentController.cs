8839d69838f533cf3478152fe0092c43a645acbd
Chintana Meegamarachchi <chintana@appsolve.com.au>
2020-06-22 12:33:12 +0800

    Implements WebAPI

--- AppointmentsManager.WebAPI/Controllers/AppointmentController.cs
+++ AppointmentsManager.WebAPI/Controllers/AppointmentController.cs
@@ -1,7 +1,99 @@
-﻿namespace AppointmentsManager.WebAPI.Controllers
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using AppointmentsManager.Application.Appointments;
+using AppointmentsManager.Domain.Appointments;
+using MediatR;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AppointmentsManager.WebAPI.Controllers
 {
-    public class AppointmentController
+    [ApiController]
+    [Route("[controller]")]
+    public class AppointmentController: ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public AppointmentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<AppointmentSummary[]>> Get()
+        {
+            var result =
+                await _mediator.Send(
+                    new GetAppointmentSummaryListQuery.Query());
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> Post(CreateAppointmentCommand.Command command)
+        {
+            var result =
+                await _mediator.Send(command);
+            
+            return Ok(result);
+        }
         
+        [HttpPut]
+        public async Task<ActionResult<string>> Put(UpdateAppointmentCommand.Command command)
+        {
+            var result =
+                await _mediator.Send(command);
+            
+            return Ok(result);
+        }
+
+        [HttpPatch]
+        [Route("{id}")]
+        [SuppressMessage("ReSharper", "InvertIf")]
+        [SuppressMessage("ReSharper", "UseObjectOrCollectionInitializer")]
+        public async Task<ActionResult<string>> Patch(
+            [FromBody] JsonPatchDocument<UpdateAppointmentCommand.Command> commandPatch, string id)
+        {
+            var appointment = await _mediator.Send(new GetAppointmentQuery.Query {Id = id});
+
+            if (appointment != null)
+            {
+                var command = new UpdateAppointmentCommand.Command();
+                command.Id = id;
+                command.Comments = appointment.Comments;
+                command.Place = appointment.Place;
+                command.Description = appointment.Description;
+                command.DateTime = appointment.DateTime;
+                
+                commandPatch.ApplyTo(command,ModelState);
+
+                if (ModelState.IsValid)
+                {
+                    await _mediator.Send(command);
+                }
+                else
+                {
+                    var serializableModelState = new SerializableError(ModelState);
+                    var modelStateJson = JsonConvert.SerializeObject(serializableModelState);
+                    
+                    return BadRequest(modelStateJson);
+                }
+                
+                return Ok(id);
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult> Delete(string id)
+        {
+            var command = new DeleteAppointmentCommand.Command { Id = id } ;
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
     }
 }
--- AppointmentsManager.WebAPI/Startup.cs
+++ AppointmentsManager.WebAPI/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AppointmentsManager.Storage.Appointments;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace AppointmentsManager.WebAPI
 {
@@ -25,7 +20,20 @@ namespace AppointmentsManager.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            {
+                var basePath = Configuration["FILESYSTEM_APPOINTMENT_STORAGE_BASE_FOLDER"];
+                services.AddScoped<IAppointmentStorage>((options) => new FileSystemAppointmentStorage(basePath));
+            }
+            
+            services.AddMediatR(typeof(AppointmentsManager.Application.Appointments.CreateAppointmentCommand).Assembly);
+            
+            services.AddControllers().AddNewtonsoftJson();
+            
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    policy => { policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin(); });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,11 +44,16 @@ namespace AppointmentsManager.WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            if (env.IsProduction())
+            {
+                app.UseHttpsRedirection();
+            }
 
             app.UseRouting();
 
             app.UseAuthorization();
+            
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
